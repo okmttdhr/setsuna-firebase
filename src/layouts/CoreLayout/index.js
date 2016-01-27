2e40b1a1c0a3d855038b96871f293aa86c47fad9
okmttdhr <10101114fin@gmail.com>
2016-01-27 16:58:30 +0700

    [add]usresをstateに

--- src/layouts/CoreLayout/index.js
+++ src/layouts/CoreLayout/index.js
@@ -1,14 +1,40 @@
 import 'styles/core.scss'
+import ReactFireMixin from 'reactfire'
+import reactMixin from 'react-mixin'
+import Firebase from 'firebase'
+import utils from 'utils/index'
 import styles from './index.scss'
 import Header from 'components/Header/index'
+import config from 'utils/config'
+
+const firebaseRef = new Firebase(config.firebase.demoRef)
 
 export class CoreLayout extends React.Component {
   static propTypes = {
     children: React.PropTypes.element
   }
 
+  constructor () {
+    super()
+    this.state = {
+      users: null
+    }
+  }
+
+  componentDidMount () {
+    let uid = null
+    firebaseRef.onAuth(function(authData) {
+      if (!authData) return
+      uid = authData.auth.uid
+    })
+    const refUsers = new Firebase(config.firebase.demoRef + 'users/' + uid)
+    this.bindAsObject(refUsers, 'users')
+  }
+
   render () {
-    const children = React.cloneElement(this.props.children, {apppropdesu: 'aaa'})
+    console.log('this.state')
+    console.log(this.state)
+    const children = React.cloneElement(this.props.children, {users: this.state.users})
     return (
       <div className='page-container'>
         <Header/>
@@ -20,4 +46,4 @@ export class CoreLayout extends React.Component {
   }
 }
 
-export default CoreLayout
+export default reactMixin.decorate(ReactFireMixin)(CoreLayout)
