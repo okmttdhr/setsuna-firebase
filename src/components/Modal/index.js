8f9f1a21ea86893dc9534916acae286abbbf81a7
okmttdhr <10101114fin@gmail.com>
2016-02-17 21:46:30 +0700

    css

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,5 @@
 import styles from './index.scss'
 import classNames from 'classnames'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export class Modal extends React.Component {
   static propTypes = {
@@ -16,16 +15,10 @@ export class Modal extends React.Component {
         [styles.isShow]: this.props.isShow,
       })}>
         <div className={styles.Modal__overlay} onClick={::this.props.toggleShow}></div>
-        <div className={classNames({
-          [styles.Modal__content]: true,
-          [styles['Modal__content--isShow']]: this.props.isShow,
-        })}>{this.props.children}</div>
+        <div className={styles.Modal__content}>{this.props.children}</div>
       </div>
     )
   }
 }
 
 export default Modal
-
-// })} onClick={(e) => e.stopPropagation()}>
-// <ReactCSSTransitionGroup transitionName='example' transitionAppear={this.props.isShow}>
