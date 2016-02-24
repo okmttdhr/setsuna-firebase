fc4c31920f08642af27341f9a3e295b11cd236d3
okmttdhr <10101114fin@gmail.com>
2016-02-24 17:56:48 +0700

    [add]Sharersの骨組み

--- src/components/Sharers/index.js
+++ src/components/Sharers/index.js
@@ -1,4 +1,5 @@
 import styles from './index.scss'
+import classNames from 'classnames'
 import SharerTwitter from 'components/Sharers/Twitter/index'
 import SharerFacebook from 'components/Sharers/Facebook/index'
 import SharerLine from 'components/Sharers/Line/index'
@@ -6,20 +7,43 @@ import SharerHatebu from 'components/Sharers/Hatebu/index'
 
 export default class Sharers extends React.Component {
   static propTypes = {
+    isShow: React.PropTypes.bool.isRequired,
   }
 
-  handleclick(e) {
+  constructor() {
+    super()
+    this.state = {
+      isShow: false,
+    }
+  }
+
+  toggleShow(e) {
     e.stopPropagation()
-    console.log('modalとか開く')
+    this.setState({ ...this.state, isShow: !this.state.isShow })
   }
 
   render() {
     return (
-      <div className={styles.Sharers} onClick={::this.handleclick}>
-        <SharerFacebook />
-        <SharerTwitter />
-        <SharerHatebu />
-        <SharerLine />
+      <div
+        className={classNames({
+          [styles.Sharers]: true,
+          [styles.isShow]: this.state.isShow,
+        })}
+        onClick={::this.toggleShow}>
+        <i className={classNames({
+          'material-icons': true,
+          [styles.Sharers__icon]: true,
+        })}>reply</i>
+        <div className={styles.Sharers__overlay} onClick={::this.toggleShow}></div>
+        <div
+          className={classNames({
+            [styles.Sharers__content]: true,
+          })}>
+          <SharerFacebook />
+          <SharerTwitter />
+          <SharerHatebu />
+          <SharerLine />
+        </div>
       </div>
     )
   }
