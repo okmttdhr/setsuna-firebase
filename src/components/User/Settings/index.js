9b45497a7fca3f6c096182ef33995b61016a8eb1
okmttdhr <10101114fin@gmail.com>
2016-03-15 21:22:55 +0900

    [add]UserSettingsのstyle

--- src/components/User/Settings/index.js
+++ src/components/User/Settings/index.js
@@ -1,5 +1,6 @@
 import styles from './index.scss'
 import i18next from 'i18next'
+import classNames from 'classnames'
 import firebaseUtils from 'utils/firebase/index'
 
 export default class UserSettings extends React.Component {
@@ -37,21 +38,54 @@ export default class UserSettings extends React.Component {
       return null
     }
     return (
-      <div>
-        <div>
-          <p>{userFirebase[userFirebase.auth.provider].displayName}</p>
-          <p>{userFirebase[userFirebase.auth.provider].email}</p>
-        </div>
-        <select onChange={::this._changeLang} defaultValue={i18next.language}>
-          {this.langOptions.map((option, index) => (
-              <option
-                key={index}
-                value={option.value}>
-                  {option.name}
-              </option>
-            ))}
-        </select>
-        <div onClick={::this._logout}>{i18next.t('User__logout')}</div>
+      <div className={styles.UserSettings}>
+        <ul className={styles.UserSettings__list}>
+          <li>
+            <i className={classNames({
+              [styles.UserSettings__list__icon]: true,
+              'material-icons': true,
+            })}>person</i>
+            <p className={styles.UserSettings__list__text}>
+              {userFirebase[userFirebase.auth.provider].displayName}
+            </p>
+          </li>
+          {userFirebase[userFirebase.auth.provider].email
+            ?
+              <li>
+                <i className={classNames({
+                  [styles.UserSettings__list__icon]: true,
+                  'material-icons': true,
+                })}>email</i>
+                <p className={styles.UserSettings__list__text}>
+                  {userFirebase[userFirebase.auth.provider].email}
+                </p>
+              </li> : null}
+          <li>
+            <i className={classNames({
+              [styles.UserSettings__list__icon]: true,
+              'material-icons': true,
+            })}>language</i>
+            <select
+              className={styles.UserSettings__list__select}
+              onChange={::this._changeLang}
+              defaultValue={i18next.language}>
+              {this.langOptions.map((option, index) => (
+                  <option
+                    key={index}
+                    value={option.value}>
+                      {option.name}
+                  </option>
+                ))}
+            </select>
+          </li>
+          <li className={styles.UserSettings__list__logout} onClick={::this._logout}>
+            <i className={classNames({
+              [styles.UserSettings__list__icon]: true,
+              'material-icons': true,
+            })}>skip_next</i>
+            <p className={styles.UserSettings__list__text}>{i18next.t('User__logout')}</p>
+          </li>
+        </ul>
       </div>
     )
   }
