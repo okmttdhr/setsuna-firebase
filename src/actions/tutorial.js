0e59a4e1b421d7144e5189d19e456dd8eb8b9b15
okmttdhr <10101114fin@gmail.com>
2016-03-20 20:41:15 +0900

    localStorageに追加

--- src/actions/tutorial.js
+++ src/actions/tutorial.js
@@ -1,7 +1,10 @@
 import { createAction } from 'redux-actions'
 import * as constants from 'constants'
 
-export const toggleTutorialHasDone = createAction(constants.TOGGLE_TUTORIAL_HAS_DONE, (tutorialTypeName) => tutorialTypeName)
+export const toggleTutorialHasDone = createAction(
+  constants.TOGGLE_TUTORIAL_HAS_DONE,
+  (tutorialTypeName) => tutorialTypeName
+)
 
 export default {
   toggleTutorialHasDone,
--- src/reducers/tutorial.js
+++ src/reducers/tutorial.js
@@ -2,28 +2,37 @@ import { handleActions } from 'redux-actions'
 import { tutorial as initialState } from 'reducers/initialState'
 import * as constants from 'constants'
 
+const hasDoneLocalStorage = JSON.parse(localStorage.getItem('tutorialHasDone'))
+const initialStateObject = hasDoneLocalStorage ? {
+  ...initialState,
+  hasDone: {
+    in: {
+      ...initialState.hasDone.in,
+      ...hasDoneLocalStorage.in,
+    },
+    action: {
+      ...initialState.hasDone.action,
+      ...hasDoneLocalStorage.action,
+    },
+  },
+} : initialState
+
 export default handleActions({
   [constants.TOGGLE_TUTORIAL_HAS_DONE]: (state, action) => {
     const tutorialType = action.payload.type
     const tutorialName = action.payload.name
     let hasDone
-    if (tutorialType === 'in') {
-      hasDone = {
-        in: {
-          ...state.hasDone.in,
-          [tutorialName]: !state.hasDone[tutorialName],
-        }
-      }
-    } else if (tutorialType === 'action') {
-      hasDone = {
-        action: {
-          ...state.hasDone.action,
-        }
-      }
+    hasDone = {
+      ...state.hasDone,
+      [tutorialType]: {
+        ...state.hasDone[tutorialType],
+        [tutorialName]: !state.hasDone[tutorialName],
+      },
     }
+    localStorage.setItem('tutorialHasDone', JSON.stringify(hasDone))
     return {
       ...state,
       hasDone,
     }
   },
-}, initialState)
+}, initialStateObject)
