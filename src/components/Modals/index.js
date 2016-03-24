5d5dd49b56ee84abfb1bced1a839dd44c5993197
okmttdhr <10101114fin@gmail.com>
2016-03-24 21:52:23 +0900

    move ModalPost in Modals

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import applicationActions from 'actions/application'
 import Modal from 'components/Modal/index'
 import ModalLogin from 'components/Modal/Login/index'
+import ModalPost from 'components/Modal/Post/index'
 
 const mapStateToProps = (state) => ({
   application: state.application,
@@ -11,8 +12,10 @@ const mapStateToProps = (state) => ({
 
 export class Modals extends React.Component {
   static propTypes = {
+    userFirebase: React.PropTypes.object,
     application: React.PropTypes.object.isRequired,
     toggleModalLogin: React.PropTypes.func.isRequired,
+    toggleModalPost: React.PropTypes.func.isRequired,
   }
 
   _toggleModalLogin(e) {
@@ -20,12 +23,30 @@ export class Modals extends React.Component {
     this.props.toggleModalLogin()
   }
 
+  _toggleModalPost(e) {
+    e.stopPropagation()
+    if (!this.props.userFirebase) {
+      return this.props.toggleModalLogin()
+    }
+    this.props.toggleModalPost()
+  }
+
   render() {
     const { application } = this.props
+    const contentStyleMd = {
+      backgroundColor: '#ECEDF8',
+      height: '185px',
+    }
     return (
       <div className={styles.Modals}>
         <Modal isShow={application.isModalLoginShow} toggleShow={::this._toggleModalLogin}>
           <ModalLogin {...this.props} />
+        </Modal>
+        <Modal
+          isShow={application.isModalPostShow}
+          toggleShow={::this._toggleModalPost}
+          contentStyleMd={contentStyleMd}>
+          <ModalPost {...this.props} />
         </Modal>
       </div>
     )
--- src/components/Navigation/Post/index.js
+++ src/components/Navigation/Post/index.js
@@ -1,14 +1,10 @@
 import styles from './index.scss'
 import classNames from 'classnames'
 
-import Modal from 'components/Modal/index'
-import ModalPost from 'components/Modal/Post/index'
-
 export default class NavigationPost extends React.Component {
   static propTypes = {
     userFirebase: React.PropTypes.object,
     toggleModalLogin: React.PropTypes.func.isRequired,
-    application: React.PropTypes.object.isRequired,
     toggleModalPost: React.PropTypes.func.isRequired,
   }
 
@@ -16,7 +12,7 @@ export default class NavigationPost extends React.Component {
     super()
   }
 
-  toggleModalShow(e) {
+  _toggleModalPost(e) {
     e.stopPropagation()
     if (!this.props.userFirebase) {
       return this.props.toggleModalLogin()
@@ -25,23 +21,13 @@ export default class NavigationPost extends React.Component {
   }
 
   render() {
-    const contentStyleMd = {
-      backgroundColor: '#ECEDF8',
-      height: '185px',
-    }
     return (
-      <li className={styles.NavigationPost} onClick={::this.toggleModalShow}>
+      <li className={styles.NavigationPost} onClick={::this._toggleModalPost}>
         <i className={classNames({
           [styles.NavigationPost__icon]: true,
           'material-icons': true,
         })}>edit</i>
         <div className={styles.NavigationPost__text}>Post</div>
-        <Modal
-          isShow={this.props.application.isModalPostShow}
-          toggleShow={::this.toggleModalShow}
-          contentStyleMd={contentStyleMd}>
-          <ModalPost {...this.props} />
-        </Modal>
       </li>
     )
   }
