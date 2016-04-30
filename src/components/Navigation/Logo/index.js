27b1dac993f9823ce749a906f399b99235b5fc8b
okmttdhr <10101114fin@gmail.com>
2016-04-30 10:10:13 +0900

    added to comp

--- src/components/Navigation/Logo/index.js
+++ src/components/Navigation/Logo/index.js
@@ -6,6 +6,21 @@ export default class NavigationLogo extends React.Component {
     history: React.PropTypes.object.isRequired,
   }
 
+  componentDidMount() {
+    const canvas = document.getElementById('logo__setsuna')
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d')
+      ctx.beginPath()
+      ctx.lineJoin = 'miter'
+      ctx.arc(120, 88, 35, 5.74, 3.66, false)
+      ctx.bezierCurveTo(100, 55, 122, 27.5, 120, 20)
+      ctx.bezierCurveTo(122, 27.5, 121, 31.5, 150, 70)
+      ctx.closePath()
+      ctx.fillStyle = 'rgb(63,81,181)'
+      ctx.fill()
+    }
+  }
+
   _linkTo() {
     this.props.history.pushState(null, '/')
   }
@@ -15,6 +30,7 @@ export default class NavigationLogo extends React.Component {
       <li className={classNames({
         [styles.NavigationLogo]: true,
       })} onClick={::this._linkTo}>
+        <canvas id='logo__setsuna' height='55' width='55'></canvas>
         <div className={styles.NavigationLogo__text}>Logo</div>
       </li>
     )
--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -19,21 +19,6 @@ export class HomeView extends React.Component {
     super()
   }
 
-  componentDidMount() {
-    const canvas = document.getElementById('logo__setsuna')
-    if (canvas.getContext) {
-      const ctx = canvas.getContext('2d')
-      ctx.beginPath()
-      ctx.lineJoin = 'miter'
-      ctx.arc(120, 88, 35, 5.74, 3.66, false)
-      ctx.bezierCurveTo(100, 55, 122, 27.5, 120, 20)
-      ctx.bezierCurveTo(122, 27.5, 121, 31.5, 150, 70)
-      ctx.closePath()
-      ctx.fillStyle = 'rgb(63,81,181)'
-      ctx.fill()
-    }
-  }
-
   _getStarted() {
     if (!this.props.userFirebase) {
       this.props.toggleModalLogin()
@@ -53,7 +38,6 @@ export class HomeView extends React.Component {
             {i18next.t('get_started')}
           </div>
         </div>
-        <canvas id='logo__setsuna' height='300' width='300'></canvas>
         <div className={styles.HomeView__explain}>
           <div className={styles.HomeView__explain__content}>
             {i18next.t('ModalTutorial__PostsView')}
