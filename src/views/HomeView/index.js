87c9039abf5bcb77bd76cb216e450ff66a7f8e38
okmttdhr <10101114fin@gmail.com>
2016-04-30 10:03:00 +0900

    add r: remove c

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -20,26 +20,16 @@ export class HomeView extends React.Component {
   }
 
   componentDidMount() {
-    const canvas = document.getElementById('canvas')
+    const canvas = document.getElementById('logo__setsuna')
     if (canvas.getContext) {
       const ctx = canvas.getContext('2d')
-      // const lineargradient = ctx.createRadialGradient(135, 95, 1, 135, 95, 10)
-      // lineargradient.addColorStop(0, 'white')
-      // lineargradient.addColorStop(1, '#77CCEE')
       ctx.beginPath()
       ctx.lineJoin = 'miter'
       ctx.arc(120, 88, 35, 5.74, 3.66, false)
       ctx.bezierCurveTo(100, 55, 122, 27.5, 120, 20)
       ctx.bezierCurveTo(122, 27.5, 121, 31.5, 150, 70)
       ctx.closePath()
-      // ctx.strokeStyle = 'rgb(63,81,181)'
-      // ctx.lineWidth = 1
       ctx.fillStyle = 'rgb(63,81,181)'
-      // ctx.shadowOffsetX = 2
-      // ctx.shadowOffsetY = 2
-      // ctx.shadowBlur = 2
-      // ctx.shadowColor = 'rgba(50, 50, 50, 0.5)'
-      // ctx.stroke()
       ctx.fill()
     }
   }
@@ -63,7 +53,7 @@ export class HomeView extends React.Component {
             {i18next.t('get_started')}
           </div>
         </div>
-        <canvas id='canvas' height='300' width='300'></canvas>
+        <canvas id='logo__setsuna' height='300' width='300'></canvas>
         <div className={styles.HomeView__explain}>
           <div className={styles.HomeView__explain__content}>
             {i18next.t('ModalTutorial__PostsView')}
