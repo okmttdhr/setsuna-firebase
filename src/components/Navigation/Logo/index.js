c8623bdb7b57ef960a2a67e89b8416c3f191a604
okmttdhr <10101114fin@gmail.com>
2016-04-30 10:25:12 +0900

    remove logo

--- src/components/Navigation/Logo/index.js
+++ src/components/Navigation/Logo/index.js
@@ -6,20 +6,7 @@ export default class NavigationLogo extends React.Component {
     history: React.PropTypes.object.isRequired,
   }
 
-  componentDidMount() {
-    const canvas = document.getElementById('logo__setsuna')
-    if (canvas.getContext) {
-      const ctx = canvas.getContext('2d')
-      ctx.beginPath()
-      ctx.lineJoin = 'miter'
-      ctx.arc(120, 88, 35, 5.74, 3.66, false)
-      ctx.bezierCurveTo(100, 55, 122, 27.5, 120, 20)
-      ctx.bezierCurveTo(122, 27.5, 121, 31.5, 150, 70)
-      ctx.closePath()
-      ctx.fillStyle = 'rgb(63,81,181)'
-      ctx.fill()
-    }
-  }
+  componentDidMount() {}
 
   _linkTo() {
     this.props.history.pushState(null, '/')
@@ -30,8 +17,7 @@ export default class NavigationLogo extends React.Component {
       <li className={classNames({
         [styles.NavigationLogo]: true,
       })} onClick={::this._linkTo}>
-        <canvas id='logo__setsuna' height='55' width='55'></canvas>
-        <div className={styles.NavigationLogo__text}>Logo</div>
+        <div className={styles.NavigationLogo__text}>Setsuna</div>
       </li>
     )
   }
